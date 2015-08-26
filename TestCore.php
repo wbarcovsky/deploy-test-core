b543af3512ac9233124850416be5fca853f4f9e6
G00DWIN <wbarcovsky@yandex.ru>
2015-08-26 15:57:47 +0400

    second commit

--- TestCore.php
+++ TestCore.php
@@ -9,4 +9,9 @@ class Core {
     public static function Hello() {
         print 'Hello, ' . self::$world;
     }
+
+    public function help()
+    {
+        print 'This is some new functional';
+    }
 }
